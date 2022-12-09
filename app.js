e8600d77491c61ca5ae14c97818e412534ca4c7f
Anjana Dunukara <anjanaheshanthach@gmail.com>
2022-12-09 21:26:17 +0530

    add error handling

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const url =
   "http://api.weatherstack.com/current?access_key=fe42a58169837ab002c0615d7698f253&query=37.8267,-122.4233&units=f";
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(
-    response.body.current.weather_descriptions[0] +
-      ". It is curruntly " +
-      response.body.current.temperature +
-      " degrees out. It feels like " +
-      response.body.current.feelslike +
-      " degrees out."
-  );
+  if (error) {
+    console.log("Unable to connect to weather service");
+  } else if (response.body.error) {
+    console.log("Unable to find location");
+  } else {
+    console.log(
+      response.body.current.weather_descriptions[0] +
+        ". It is curruntly " +
+        response.body.current.temperature +
+        " degrees out. It feels like " +
+        response.body.current.feelslike +
+        " degrees out."
+    );
+  }
 });
 
 const urlgeo =
@@ -20,13 +26,19 @@ const urlgeo =
 request({ url: urlgeo, json: true }, (error, response) => {
   //   console.log(response);
 
-  const geodata = response.body.features[0];
+  if (error) {
+    console.log("Unable to connect to mapbox API");
+  } else if (!response.body.features[0]) {
+    console.log("Unable to find location you searched");
+  } else {
+    const geodata = response.body.features[0];
 
-  console.log(
-    "Your lattidude: " +
-      geodata.center[1] +
-      " Your longtitude: " +
-      geodata.center[0] +
-      ""
-  );
+    console.log(
+      "Your lattidude: " +
+        geodata.center[1] +
+        " Your longtitude: " +
+        geodata.center[0] +
+        ""
+    );
+  }
 });
