d5bca330296d92e2a5008f583099a1e143fb88f7
Anjana Dunukara <anjanaheshanthach@gmail.com>
2022-12-09 21:02:53 +0530

    Add weatherstack and mapbox api urls

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("2 second timer");
-}, 2000);
+const url =
+  "http://api.weatherstack.com/current?access_key=fe42a58169837ab002c0615d7698f253&query=37.8267,-122.4233&units=f";
 
-console.log("Stopping");
+request({ url: url, json: true }, (error, response) => {
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is curruntly " +
+      response.body.current.temperature +
+      " degrees out. It feels like " +
+      response.body.current.feelslike +
+      " degrees out."
+  );
+});
+
+const urlgeo =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZHVudTk3IiwiYSI6ImNsYmdsNnZlajBoNzMzcHBkOWtxc20xZDMifQ.-X2t0qoTnKLm_apQ-R3jQw&limit=1";
+
+request({ url: urlgeo, json: true }, (error, response) => {
+  //   console.log(response);
+
+  const geodata = response.body.features[0];
+
+  console.log(
+    "Your lattidude: " +
+      geodata.center[1] +
+      " Your longtitude: " +
+      geodata.center[0] +
+      ""
+  );
+});
