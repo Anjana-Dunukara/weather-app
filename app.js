c3e50c897e811362ec08e60d0449f4f2eef21152
Anjana Dunukara <anjanaheshanthach@gmail.com>
2022-12-13 19:16:31 +0530

    refine the code

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const forecast = require("./utils/forecast");
 const location = process.argv[2];
 
 if (location) {
-  geocode(location, (error, { latitude, longtitude, location }) => {
+  //Adding a empty object after response(Line 11) object destructuring because if error occurs response will be undifine,
+  //so if node tries to destructure undifine object it gonna throw a error if you provide a empty object, if there is no values to destructure
+  //it will automatically initialize the default value in that object in this case empty object(undefine to the each prperty).
+
+  geocode(location, (error, { latitude, longtitude, location } = {}) => {
     if (error) {
       return console.log("Error: ", error);
     }
